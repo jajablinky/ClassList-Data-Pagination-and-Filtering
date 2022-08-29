c0eac60ed439d93f7e75bfb1ecf0fe3901dd6955
jajablinky <gmillar6@gmail.com>
2022-08-29 16:41:37 -0700

    stuck on creating elements for DOM. Not sure how to store data files or select.

--- js/script.js
+++ js/script.js
@@ -16,10 +16,24 @@ For assistance:
 /*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
+A list AND page parameter that will be passed as an argument.
+
 */
 
+function showPage (list, page) {
+const startIndex = ( page * 9 ) - 9
+const endIndex = page * 9;
+const studentList = document.querySelector('UL.student-list');
+studentList.innerHTML = '';
+for (i = 0; i < list.length; i++) {
+   if( i >= startIndex && i < endIndex) {
+      const studentItem = `${data[i]}`
+   } 
+
+}
 
 
+};
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
@@ -28,3 +42,4 @@ This function will create and insert/append the elements needed for the paginati
 
 
 // Call functions
+
